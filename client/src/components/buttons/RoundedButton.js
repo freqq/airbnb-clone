6f14fc32b60842940a775ee2ceae51516c4543ef
Przemyslaw Wit <dobrzeprzemyslane@gmail.com>
2019-03-29 12:02:39 +0100

    Finished main screen

--- client/src/components/buttons/RoundedButton.js
+++ client/src/components/buttons/RoundedButton.js
@@ -1,39 +1,55 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import colors from '../../styles/colors'
-import {
-    Text,
-    View,
-    TouchableHighlight,
-    StyleSheet
-} from 'react-native'
+import {Text, View, TouchableHighlight, StyleSheet} from 'react-native'
 
 export default class RoundedButton extends Component {
     render() {
-        const { text, color, background } = this.props;
+        const {text, textColor, icon, background, handleOnPress} = this.props;
+        const backgroundColor = background || "transparent"
+        const color = textColor || colors.black
         return (
-            <TouchableHighlight style={styles.wrapper}>
-                <Text style={[{color},styles.buttonText]}>{text}</Text>
+            <TouchableHighlight
+                style={[{
+                    backgroundColor
+                }, styles.wrapper]}
+                onPress={handleOnPress}>
+                <View style={styles.buttonTextWrapper}>
+                    {icon}
+                    <Text
+                        style={[{
+                            color
+                        }, styles.buttonText]}>{text}</Text>
+                </View>
             </TouchableHighlight>
         );
     }
 }
 
-RoundedButton.PropTypes = {
-    text: PropTypes.string.isRequired
+RoundedButton.propTypes = {
+    text: PropTypes.string.isRequired,
+    textColor: PropTypes.string,
+    background: PropTypes.string,
+    icon: PropTypes.object,
+    handleOnPress: PropTypes.func.isRequired
 };
 
 const styles = StyleSheet.create({
-   wrapper: {
+    wrapper: {
         display: 'flex',
         padding: 15,
         borderRadius: 40,
         borderWidth: 1,
-        borderColor: colors.white
-   },
-   buttonText:{
-       fontSize: 16,
-       width: '100%',
-       textAlign: 'center'
-   } 
+        borderColor: colors.white,
+        marginBottom: 20
+    },
+    buttonText: {
+        fontSize: 16,
+        width: '100%',
+        textAlign: 'center'
+    },
+    buttonTextWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    }
 });
--- client/src/screens/LoggedOut.js
+++ client/src/screens/LoggedOut.js
@@ -1,23 +1,62 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {StyleSheet, Text, View, Image, TouchableHighlight} from 'react-native';
 import colors from '../styles/colors'
 import RoundedButton from '../components/buttons/RoundedButton'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default class App extends Component {
-  render() {
-     return (
-         <View style={styles.wrapper}>
-            <View style={styles.welcomeWrapper}>
-             <Image 
-                source={require('../img/airbnb-logo.png')}
-                style={styles.logo}
-            />
-            <Text style={styles.welcomeText}>Welcome to Airbnb.</Text>
-            <RoundedButton text="Continue with Facebook"/>
+    onFacebookPress() {
+        alert('Facebook')
+    }
+
+    onCreateAccountPress() {
+        alert('Account')
+    }
+
+    onMoreOptionsPress() {
+        alert('More options')
+    }
+
+    render() {
+        return (
+            <View style={styles.wrapper}>
+                <View style={styles.welcomeWrapper}>
+                    <Image source={require('../img/airbnb-logo.png')} style={styles.logo}/>
+                    <Text style={styles.welcomeText}>Welcome to Airbnb.</Text>
+                    <RoundedButton
+                        text="Continue with Facebook"
+                        textColor={colors.green01}
+                        background={colors.white}
+                        icon={< Icon name = "facebook" size = {
+                        20
+                    }
+                    style = {
+                        styles.facebookButtonIcon
+                    } />}
+                        handleOnPress={this.onFacebookPress}/>
+
+                    <RoundedButton
+                        text="Create account"
+                        textColor={colors.white}
+                        handleOnPress={this.onCreateAccountPress}/>
+
+                    <TouchableHighlight 
+                        style={styles.moreOptionsButton}
+                        onPress={this.onMoreOptionsPress}>
+                        <Text style={styles.moreOptionsButtonText}>More options</Text>
+                    </TouchableHighlight>
+
+                    <View style={styles.termsAndConditions}>
+                        <Text style={styles.termsText}>
+                            Bt tapping Continute, Create Account or More options, I agree to Airbnb's 
+                             <Text style={styles.underlineText}> Terms of Service</Text>, <Text style={styles.underlineText}> Payments Terms of Service</Text>, 
+                             <Text style={styles.underlineText}> Privacy Policy</Text>, and <Text style={styles.underlineText}> Nondiscrimination Policy</Text>.
+                        </Text>
+                    </View>
+                </View>
             </View>
-         </View>
-     )
-  }
+        )
+    }
 }
 
 const styles = StyleSheet.create({
@@ -43,5 +82,32 @@ const styles = StyleSheet.create({
         color: colors.white,
         fontWeight: '300',
         marginBottom: 40
+    },
+    facebookButtonIcon: {
+        color: colors.green01,
+        position: 'relative',
+        left: 20,
+        zIndex: 8
+    },
+    moreOptionsButton: {
+        marginTop: 10
+    },
+    moreOptionsButtonText: {
+        color: colors.white,
+        fontSize: 16
+    },
+    termsText: {
+        color: colors.white,
+        fontSize: 12,
+        fontWeight: '600'
+    },
+    termsAndConditions: {
+        flexWrap: 'wrap',
+        alignItems: 'flex-start',
+        flexDirection: 'row',
+        marginTop: 30
+    },
+    underlineText: {
+        textDecorationLine: 'underline'
     }
 })
